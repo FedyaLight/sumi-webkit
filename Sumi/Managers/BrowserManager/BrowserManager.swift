b3fc1fc9fe5e72e9b701eb66290f2e63a8fff884
FedyaLight <lightfedya@gmail.com>
2026-06-30 19:12:32 +0300

    Move floating bar facade out of browser manager

--- Sumi/Managers/BrowserManager/BrowserManager.swift
+++ Sumi/Managers/BrowserManager/BrowserManager.swift
@@ -179,7 +179,7 @@ class BrowserManager: ObservableObject {
     lazy var keyboardShortcutCommandOwner = BrowserKeyboardShortcutCommandOwner(
         dependencies: .live(browserManager: self)
     )
-    private lazy var floatingBarRoutingOwner = BrowserFloatingBarRoutingOwner(
+    lazy var floatingBarRoutingOwner = BrowserFloatingBarRoutingOwner(
         dependencies: .live(browserManager: self)
     )
     lazy var floatingBarBrowserContextOwner = BrowserFloatingBarBrowserContextOwner(
@@ -602,53 +602,6 @@ class BrowserManager: ObservableObject {
         BrowserTabSelectionOwner.liveActions(for: self)
     }
 
-    func focusFloatingBarForActiveWindow(
-        prefill: String = "",
-        navigateCurrentTab: Bool = false,
-        presentationReason: FloatingBarPresentationReason = .keyboard
-    ) {
-        floatingBarRoutingOwner.focusFloatingBarForActiveWindow(
-            prefill: prefill,
-            navigateCurrentTab: navigateCurrentTab,
-            presentationReason: presentationReason
-        )
-    }
-
-    func focusFloatingBar(
-        in windowState: BrowserWindowState,
-        prefill: String = "",
-        navigateCurrentTab: Bool = false,
-        presentationReason: FloatingBarPresentationReason = .keyboard
-    ) {
-        floatingBarRoutingOwner.focusFloatingBar(
-            in: windowState,
-            prefill: prefill,
-            navigateCurrentTab: navigateCurrentTab,
-            presentationReason: presentationReason
-        )
-    }
-
-    func focusFloatingBar(
-        in windowState: BrowserWindowState,
-        prefill: String,
-        navigateCurrentTab: Bool
-    ) {
-        focusFloatingBar(
-            in: windowState,
-            prefill: prefill,
-            navigateCurrentTab: navigateCurrentTab,
-            presentationReason: .keyboard
-        )
-    }
-
-    func showNewTabFloatingBar(in windowState: BrowserWindowState) {
-        floatingBarRoutingOwner.showNewTabFloatingBar(in: windowState)
-    }
-
-    func openNewTabOrFloatingBar(in windowState: BrowserWindowState) {
-        floatingBarRoutingOwner.openNewTabOrFloatingBar(in: windowState)
-    }
-
     func spaceForSidebarActions(in windowState: BrowserWindowState) -> Space? {
         sidebarActionOwner.spaceForSidebarActions(in: windowState)
     }
@@ -667,85 +620,6 @@ class BrowserManager: ObservableObject {
 
     func createGitHubIssuesLiveFolderInCurrentSpace(in windowState: BrowserWindowState) {
         sidebarActionOwner.createGitHubIssuesLiveFolderInCurrentSpace(in: windowState)
-    }
-
-    func updateFloatingBarDraft(
-        in windowState: BrowserWindowState,
-        text: String
-    ) {
-        floatingBarRoutingOwner.updateFloatingBarDraft(
-            in: windowState,
-            text: text
-        )
-    }
-
-    func dismissFloatingBar(
-        in windowState: BrowserWindowState,
-        preserveDraft: Bool,
-        cancelEmptySplitPlaceholder: Bool = true
-    ) {
-        floatingBarRoutingOwner.dismissFloatingBar(
-            in: windowState,
-            preserveDraft: preserveDraft,
-            cancelEmptySplitPlaceholder: cancelEmptySplitPlaceholder
-        )
-    }
-
-    func dismissFloatingBarForActiveWindow(preserveDraft: Bool = true) {
-        floatingBarRoutingOwner.dismissFloatingBarForActiveWindow(preserveDraft: preserveDraft)
-    }
-
-    @discardableResult
-    func dismissFloatingBarIfVisible(
-        in windowId: UUID,
-        preserveDraft: Bool = true
-    ) -> Bool {
-        floatingBarRoutingOwner.dismissFloatingBarIfVisible(
-            in: windowId,
-            preserveDraft: preserveDraft
-        )
-    }
-
-    func floatingBarCommitNavigatesCurrentTab(in windowState: BrowserWindowState) -> Bool {
-        floatingBarRoutingOwner.floatingBarCommitNavigatesCurrentTab(in: windowState)
-    }
-
-    func commitFloatingBarSuggestion(
-        _ suggestion: SearchManager.SearchSuggestion,
-        in windowState: BrowserWindowState
-    ) {
-        floatingBarRoutingOwner.commitFloatingBarSuggestion(
-            suggestion,
-            in: windowState
-        )
-    }
-
-    func commitFloatingBarNavigation(
-        to urlString: String,
-        in windowState: BrowserWindowState
-    ) {
-        floatingBarRoutingOwner.commitFloatingBarNavigation(
-            to: urlString,
-            in: windowState
-        )
-    }
-
-    func openFloatingBarSuggestion(
-        _ suggestion: SearchManager.SearchSuggestion,
-        in windowState: BrowserWindowState
-    ) {
-        floatingBarRoutingOwner.openFloatingBarSuggestion(
-            suggestion,
-            in: windowState
-        )
-    }
-
-    func dismissFloatingBarAfterSelection(in windowState: BrowserWindowState) {
-        floatingBarRoutingOwner.dismissFloatingBarAfterSelection(in: windowState)
-    }
-
-    func sanitizeFloatingBarState(in windowState: BrowserWindowState) {
-        floatingBarRoutingOwner.sanitizeFloatingBarState(in: windowState)
     }
 
     func showFindBar() {
